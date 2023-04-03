d5988ab39d1716b79ff986ccf63f1d5575f05573
mirian228 <pariksini@mail.ru>
2023-04-03 11:13:30 +0400

    Spring boot project generated using initializer

---
+++ src/main/java/com/springboot/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+		System.out.println("Spring boot");
+	}
+
+}
